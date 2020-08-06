9c2335e41757f74b00b636b6b8a7220fdfeca9f2
IonianIronist <36776432+IonianIronist@users.noreply.github.com>
2020-08-06 15:05:13 +0300

    tidying up

--- dss.py
+++ dss.py
@@ -49,104 +49,19 @@ window = Tk()
 window.title("Welcome to this DSS")
 
 window.geometry('300x800')
+texts = ["Age", "Sex", "Steroid", "Antivirals", "Fatigue", "Malaise",
+         "Anorexia", "Liver Big", " Liver Firm", "Spleen Palpable",
+        "Spiders", "Ascites", "Varices", "Bilirubin", "Alk Phosphate",
+        "SGOT", "Albumin", "Protime", "Histology"]
+vals = []
+for tex in texts:
+    lbl = Label(window, text=tex)
+    lbl.grid(column=0, row=texts.index(tex))
+    vals.append(Entry(window, width=10))
+    vals[-1].grid(column=1, row=texts.index(tex))
 
-lbl = Label(window, text="Age")
-lbl.grid(column=0, row=0)
-age = Entry(window,width=10)
-age.grid(column=1, row=0)
-
-lbl = Label(window, text="Sex")
-lbl.grid(column=0, row=1)
-sex = Entry(window,width=10)
-sex.grid(column=1, row=1)
-
-lbl = Label(window, text="Steroid")
-lbl.grid(column=0, row=2)
-ster = Entry(window,width=10)
-ster.grid(column=1, row=2)
-
-lbl = Label(window, text="Antivirals")
-lbl.grid(column=0, row=3)
-anti = Entry(window,width=10)
-anti.grid(column=1, row=3)
-
-lbl = Label(window, text="Fatigue")
-lbl.grid(column=0, row=4)
-fat = Entry(window,width=10)
-fat.grid(column=1, row=4)
-
-lbl = Label(window, text="Malaise")
-lbl.grid(column=0, row=5)
-mal = Entry(window,width=10)
-mal.grid(column=1, row=5)
-
-lbl = Label(window, text="Anorexia")
-lbl.grid(column=0, row=6)
-ano = Entry(window,width=10)
-ano.grid(column=1, row=6)
-
-lbl = Label(window, text="Liver Big")
-lbl.grid(column=0, row=7)
-livb = Entry(window,width=10)
-livb.grid(column=1, row=7)
-
-lbl = Label(window, text="Liver Firm")
-lbl.grid(column=0, row=8)
-livf = Entry(window,width=10)
-livf.grid(column=1, row=8)
-
-lbl = Label(window, text="Spleen Palpable")
-lbl.grid(column=0, row=9)
-spl = Entry(window,width=10)
-spl.grid(column=1, row=9)
-
-lbl = Label(window, text="Spiders")
-lbl.grid(column=0, row=10)
-spi = Entry(window,width=10)
-spi.grid(column=1, row=10)
-
-lbl = Label(window, text="Ascites")
-lbl.grid(column=0, row=11)
-asc = Entry(window,width=10)
-asc.grid(column=1, row=11)
-
-lbl = Label(window, text="Varices")
-lbl.grid(column=0, row=12)
-var = Entry(window,width=10)
-var.grid(column=1, row=12)
-
-lbl = Label(window, text="Bilirubin")
-lbl.grid(column=0, row=13)
-bil = Entry(window,width=10)
-bil.grid(column=1, row=13)
-
-lbl = Label(window, text="Alk Phosphate")
-lbl.grid(column=0, row=14)
-alk = Entry(window,width=10)
-alk.grid(column=1, row=14)
-
-lbl = Label(window, text="SGOT")
-lbl.grid(column=0, row=15)
-sgo = Entry(window,width=10)
-sgo.grid(column=1, row=15)
-
-lbl = Label(window, text="Albumin")
-lbl.grid(column=0, row=16)
-alb = Entry(window,width=10)
-alb.grid(column=1, row=16)
-
-lbl = Label(window, text="Protime")
-lbl.grid(column=0, row=17)
-pro = Entry(window,width=10)
-pro.grid(column=1, row=17)
-
-lbl = Label(window, text="Histology")
-lbl.grid(column=0, row=18)
-hist = Entry(window,width=10)
-hist.grid(column=1, row=18)
 
 def clicked():
-    vals = [age,sex,ster,anti,fat,mal,ano,livb,livf,spl,spi,asc,var,bil,alk,sgo,alb,pro,hist]
     vals = list(map(lambda x : x.get(), vals))
     vals.append('LIVE')
  #    vals = ['30','male','no','no','no', 'no','no','no', 'no', 'no','no', 'no',
